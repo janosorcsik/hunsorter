0eb0e7837f304202ad627c5846fdd60071d2de42
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2021-02-05 20:32:47 +0100

    🎉 First commit

---
+++ src/cleaner/accent.ts
@@ -0,0 +1,13 @@
+import {accents} from '../mapping';
+
+const clean = (text: string) => {
+	let cleaned = '';
+
+	for (const char of text) {
+		cleaned += accents[char] || char;
+	}
+
+	return cleaned;
+};
+
+export default clean;
