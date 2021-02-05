4920cca8395c082a77e24271a52be23a04a121ec
János Orcsik <orcsikj@gmail.com>
2021-02-05 21:37:57 +0100

    🎨 Reformat code with space

--- src/comparer/number.ts
+++ src/comparer/number.ts
@@ -1,13 +1,13 @@
 const compare = (i: number, j: number): number => {
-	if (i < j) {
-		return -1;
-	}
+  if (i < j) {
+    return -1;
+  }
 
-	if (i > j) {
-		return 1;
-	}
+  if (i > j) {
+    return 1;
+  }
 
-	return 0;
+  return 0;
 };
 
 export default compare;
