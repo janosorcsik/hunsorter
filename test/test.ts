6f5f3cc566efd72fb66c40a3f2c8451c6b1e7d91
János Orcsik <orcsikj@gmail.com>
2021-08-10 19:29:09 +0200

    fix type

--- test/test.ts
+++ test/test.ts
@@ -265,7 +265,7 @@ const shuffle = <T>(array: T[]) => {
 	for (let i = copy.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
 
-		[copy[i], copy[j]] = [copy[j], copy[i]];
+		[copy[i], copy[j]] = [copy[j]!, copy[i]!];
 	}
 
 	return copy;
